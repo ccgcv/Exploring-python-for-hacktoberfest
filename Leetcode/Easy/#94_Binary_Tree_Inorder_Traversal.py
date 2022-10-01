e41e0f343b38a7a7022d4930929dd6d6423f7edf
Somanshu693 <wilsonsam693@gmail.com>
2022-10-01 12:27:55 +0530

    added Leetcode folder

---
+++ Leetcode/Easy/#94_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,24 @@
+# Recursive solution
+def inorderTraversal1(self, root):
+    res = []
+    self.helper(root, res)
+    return res
+    
+def helper(self, root, res):
+    if root:
+        self.helper(root.left, res)
+        res.append(root.val)
+        self.helper(root.right, res)
+ 
+# Iterative Solution       
+def inorderTraversal(self, root):
+    res, stack = [], []
+    while True:
+        while root:
+            stack.append(root)
+            root = root.left
+        if not stack:
+            return res
+        node = stack.pop()
+        res.append(node.val)
+        root = node.right
